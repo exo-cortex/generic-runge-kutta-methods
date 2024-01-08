5e9e05563f37740eee744d3481e5a9f74357d556
lukipuki <lukipuki@physik.tu-berlin.de>
2024-01-08 23:24:23 +0100

    Fix important (physical) error in double pendulum equations.

--- src/double_pendulum.rs
+++ src/double_pendulum.rs
@@ -1,7 +1,7 @@
 use crate::integration::{rk, State, System, Tableau, Zero};
 use derive_more::{Add, AddAssign, Div, Mul};
 
-// double pendulum code adapted from here:
+// double pendulum code adapted from equations here:
 // https://diego.assencio.com/?index=e5ac36fcb129ce95a61f8e8ce0572dbf
 
 pub struct DynamicalSystem {} // empty struct
@@ -10,29 +10,24 @@ impl System for DynamicalSystem {
     type StateT = DoublePendulumState;
     type ParamT = Parameters;
     fn f(i: &Self::StateT, p: &Self::ParamT) -> Self::StateT {
-        let h1 = i.p1 * i.p2 * (i.phi1 - i.phi2).sin() / p.l1
-            * p.l2
-            * (p.m1 + p.m2 * (i.phi1 - i.phi2).sin().powi(2));
+        let phi_diff = i.phi1 - i.phi2;
+
+        let h1 =
+            i.p1 * i.p2 * phi_diff.sin() / p.l1 * p.l2 * (p.m1 + p.m2 * phi_diff.sin().powi(2));
 
         let h2 = (p.m2 * p.l2.powi(2) * i.p1.powi(2) + (p.m1 + p.m2) * p.l1.powi(2) * i.p2.powi(2)
-            - 2.0 * p.m2 * p.l1 * p.l2 * i.p1 * i.p2 * (i.phi1 - i.phi2).cos())
-            / (2.0
-                * p.l1.powi(2)
-                * p.l2.powi(2)
-                * (p.m1 + p.m2 * (i.phi1 - i.phi2).sin().powi(2)).powi(2));
+            - 2.0 * p.m2 * p.l1 * p.l2 * i.p1 * i.p2 * phi_diff.cos())
+            / (2.0 * p.l1.powi(2) * p.l2.powi(2) * (p.m1 + p.m2 * phi_diff.sin().powi(2)).powi(2));
 
-        let phi_diff = i.phi1 - i.phi2;
         DoublePendulumState {
             phi1: (p.l2 * i.p1 - p.l1 * i.p2 * phi_diff.cos())
                 / (p.l1.powi(2) * p.l2 * (p.m1 + p.m2 * phi_diff.sin().powi(2))), // check
-            p1: -(p.m1 - p.m2) * p.g * p.l1 * i.phi1.powi(2) - h1
-                + h2 * (2.0 * (i.phi1 - i.phi2)).sin(),
-            phi2: (-p.m2 * p.l2 * i.p1 * (i.phi1 - i.phi2).cos() + (p.m1 + p.m2) * p.l1 * i.p2)
-                / p.m2
+            p1: -(p.m1 + p.m2) * p.g * p.l1 * i.phi1.sin() - h1 + h2 * (2.0 * phi_diff).sin(), // check
+            phi2: (-p.m2 * p.l2 * i.p1 * phi_diff.cos() + (p.m1 + p.m2) * p.l1 * i.p2) / p.m2
                 * p.l1
                 * p.l2.powi(2)
-                * (p.m1 + p.m2 * (i.phi1 - i.phi2).sin().powi(2)),
-            p2: -p.m2 * p.g * p.l2 * i.phi2.sin() + h1 - h2 * (2.0 * (i.phi1 - i.phi2)).sin(),
+                * (p.m1 + p.m2 * phi_diff.sin().powi(2)),
+            p2: -p.m2 * p.g * p.l2 * i.phi2.sin() + h1 - h2 * (2.0 * phi_diff).sin(),
         }
     }
 }

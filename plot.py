4a1c2425af57b9b735eb3689f30d5c1af2cc181d
lukipuki <lukipuki@physik.tu-berlin.de>
2024-01-08 14:39:23 +0100

    Update 'plot.py'.
    Added argparse to select which file's contents shall be plottet.
    e.g. use with option "$ ./plot.py -f 0 1" to only plot files 0 and 1.

--- plot.py
+++ plot.py
@@ -3,9 +3,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--files', nargs='*', required=False, type=int, default=[-1], help="list of files to plot")
+args = parser.parse_args()
 
 filenames = glob.glob("./*.dat")
 print(filenames)
+
+if args.files != [-1]:
+    filenames_temp = []
+    for (i,filename) in enumerate(filenames):
+        if i in args.files:
+            filenames_temp += [filename]
+    filenames = filenames_temp
 
 files = [np.loadtxt(filename) for filename in filenames]
 
@@ -15,14 +27,16 @@ def pos2(ang1, ang2):
 def pos2(ang1, ang2):
     return [cos(ang1) + cos(ang2), sin(ang1) + sin(ang2)]
 
-for (name, file) in zip(filenames,files):
-    # pos1 = pos1(file[:,])
 
+line_colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
+for (i,(name, file)) in enumerate(zip(filenames,files)):
+    color = line_colors[i %  10]
     # plt.plot(file[:,0], np.log(file[:, 1]), label=name)
-    # plt.plot(file[:,0], file[:, 1], label=name)
-    plt.plot(file[:,0], file[:, 2], label="p1 in {}".format(name))
-    # plt.plot(file[:,0], file[:, 3])
-    # plt.plot(file[:,0], file[:, 4])
+    plt.plot(file[:,0], (file[:, 1] + np.pi) % ( 2.0 * np.pi) - np.pi, "-", color=color, label="phi1 in {}".format(name))
+    plt.plot(file[:,0], 7 + file[:, 2], "-", color=color, label="p1 in {}".format(name))
+    plt.plot(file[:,0], 21 + (file[:, 3] + np.pi) % ( 2.0 * np.pi) - np.pi, "--", color=color, label="phi2 in {}".format(name))
+    plt.plot(file[:,0], 35 + file[:, 4],"--", color=color, label="p2 in {}".format(name))
 
 plt.legend()
 plt.show()
